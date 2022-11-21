e75603b8de05f3220802eadbeb4d94731f678d32
= <=>
2022-11-21 20:11:18 +0900

    두번쨰 커밋

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class Sum {
+    public int getSum(int a, int b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HI");
+        System.out.println("두번째 커밋");
+    }
+}
---
+++ src/SumTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+    @org.junit.Test
+    public void getSum() {
+        assertEquals(8, 5 + 3);
+    }
+}
